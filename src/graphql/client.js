1d9ad88e1449f30dbefea4f7a047f18a76f7d64c
Jaspreet Singh <jaspreet_xps@yahoo.com>
2018-02-20 13:01:15 -0800

    Update client.js

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,6 +1,4 @@
 import ApolloClient from 'apollo-boost';
-// axios used as an example here, it has nothing to do with the actual graphql code
-import axios from 'axios';
 
 const client = new ApolloClient(({
   uri: 'https://www.graphqlhub.com/graphql',
@@ -13,10 +11,8 @@ const client = new ApolloClient(({
       Mutation: {
         // this mutation can do an arbitrary number of things inside this function
         // from a redux background, we can achieve updating the local store with `cache.writeData`
-        // from a saga perspective, the mutation name is the "topic", and we can execute arbitary code inside this function (even async code as the example is doing right now)
-        addMeme: async (root, args, { cache }) => {
-          const randomFetch = await axios.get('https://swapi.co/api/people/1');
-          console.log(randomFetch);
+        // from a saga perspective, the mutation name is the "topic", and we can execute arbitary code inside this function (even async code)
+        addMeme: (root, args, { cache }) => {
           const currMemes = cache.data.data.ROOT_QUERY.numMemes;
           const numMemes = currMemes + 1;
           cache.writeData({ data: { numMemes } });
